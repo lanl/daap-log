9744eada84a3eba4e8c969763c70ef4b47258806
Hugh N. Greenberg <hng@lanl.gov>
2020-03-31 10:54:03 -0600

    Removed print statements

--- daap_launch.sh
+++ daap_launch.sh
@@ -2,9 +2,9 @@
 
 #SBATCH --job-name=IOR_DAAP
 #SBATCH --output=IOR_DAPP_%j.out
-#SBATCH --nodes=2              # Number of nodes
+#SBATCH --nodes=4              # Number of nodes
 #SBATCH --ntasks=4             # Number of MPI ranks
-#SBATCH --ntasks-per-node=2    # Number of MPI ranks per node
+#SBATCH --ntasks-per-node=1    # Number of MPI ranks per node
 #SBATCH --ntasks-per-socket=1  # Number of tasks per processor socket on the node
 #SBATCH --time=01:00:00      # Walltime in hh:mm:ss or d-hh:mm:ss
 
@@ -16,15 +16,11 @@ function launch_fluent_bit {
     echo $SLURM_HOSTS
     IFS=',' read -ra host_array <<< "$SLURM_HOSTS"
     host_array_len=${#host_array[@]}
-    echo $host_array_len
     num_servers=`echo $host_array_len/4.0 | bc -l | awk '{printf("%d\n",$1 + 0.5)}'`
     servers=()
-    echo $num_servers
     for h in "${host_array[@]}"
     do
-	echo ${h}
 	host_ip=`cat /etc/hosts | grep "[[:space:]]${h}\." | cut -f 1 -d$'\t'`
-	echo ${host_ip}
 	servers_array_len=${#servers[@]}
 	if [ $servers_array_len != $num_servers ]
 	then
@@ -50,6 +46,6 @@ launch_fluent_bit
 module load openmpi/2.1.2
 
 #Run the job
-sleep 20 && srun /users/hng/ior-daap/src/ior -t 1m -b 1g
+sleep 20 && srun /users/hng/ior-daap/src/ior -t 1m -b 1g -o /lustre/scratch3/yellow/hng/testFile -i 100
 sleep 60
 date
