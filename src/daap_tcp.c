88f539e628a266dae09d0d57d28dd22a0702a65e
Charles Shereda <cpshereda@lanl.gov>
2020-07-13 10:15:06 -0600

    Modified TCP/SSL functionality to improve performance.
    Signed-off-by: Charles Shereda <cpshereda@lanl.gov>

--- src/daap_tcp.c
+++ src/daap_tcp.c
@@ -33,31 +33,83 @@ static pthread_mutex_t write_mutex = PTHREAD_MUTEX_INITIALIZER;
 SSL *cSSL = NULL;
 int sockfd = -1;
 
-void daapInitializeSSL() {
+static SSL_CTX *sslctx;
+
+int daapInitializeSSL() {
+  char ssl_client_cert[PATH_MAX];
+  char ssl_client_key[PATH_MAX];
+  int home_len;
+  int ssl_client_key_len;
+  int ssl_client_cert_len;
+  char *home;
+  long options;
+
   SSL_load_error_strings();
   SSL_library_init();
   OpenSSL_add_all_algorithms();
+
+  home = getenv("HOME");
+  ssl_client_cert_len = strlen(SSL_CLIENT_CERT);
+  ssl_client_key_len = strlen(SSL_CLIENT_KEY);
+  if (home != NULL && (strlen(home) + ssl_client_key_len < PATH_MAX) && 
+      (strlen(home) + ssl_client_cert_len < PATH_MAX)) {
+    home_len = strlen(home);
+    snprintf(ssl_client_cert, home_len+ssl_client_cert_len+2,"%s%s", home, SSL_CLIENT_CERT);
+    snprintf(ssl_client_key, home_len+ssl_client_key_len+2,"%s%s", home, SSL_CLIENT_KEY);
+    if (access(ssl_client_cert, F_OK) == -1 || access(ssl_client_key, F_OK) == -1) {
+      fprintf(stderr, "Cannot find DAAP ssl cert: %s or key: %s\n", 
+	      ssl_client_cert, ssl_client_key);
+      return -1;
+    }
+  } else{
+      fprintf(stderr, "Invalid path length for certificates\n");
+      return -1;
+  }
+
+  // create context, which will remain the same throughout the run
+  // note: check to see that it is properly destroyed somewhere
+  // (this code moved from daapTCPConnect())
+  sslctx = SSL_CTX_new(SSLv23_client_method());
+  if (sslctx == NULL) {
+    ERROR_OUTPUT(("Error in creation of SSL_CTX object"));
+    return -1;
+  }
+  SSL_CTX_clear_options(sslctx, options);
+  // recommended to not allow use of SSLv3 protocol for security reasons, so we turn it off
+  options = SSL_OP_NO_SSLv3;
+  SSL_CTX_set_options(sslctx, options);
+  int use_cert = SSL_CTX_use_certificate_file(sslctx, ssl_client_cert, 
+					      SSL_FILETYPE_PEM);
+  int use_prv = SSL_CTX_use_PrivateKey_file(sslctx, ssl_client_key, 
+					    SSL_FILETYPE_PEM);
+  SSL_CTX_set_verify(sslctx, SSL_VERIFY_NONE, NULL); 
 }
 
+// function below integrated into daapShutdownSSL
+/*
 void daapDestroySSL() {
   ERR_free_strings();
   EVP_cleanup();
 }
-
+*/
 void daapShutdownSSL() {
-  // One SSL_shutdown sends close_notify alert, other receives response from peer
+  SSL_CTX_free(sslctx);
+  // One SSL_shutdown sends close_notify alert, other receives response from peer (server)
   SSL_shutdown(cSSL);
   SSL_shutdown(cSSL);
   SSL_free(cSSL);
+  // migrated from daapDestroySSL()
+  ERR_free_strings();
+  EVP_cleanup();
 }
-
+/*
 int daapTCPClose() {
     // Close connection if using TCP
     daapShutdownSSL();
     daapDestroySSL();
     close(sockfd);
 }
-
+*/
 int daapTCPLogWrite(char *buf, int buf_size) {
     int count = 0, total_count = 0;
     int retries = 0;
@@ -74,7 +126,11 @@ int daapTCPLogWrite(char *buf, int buf_size) {
     while (total_count != buf_size) {
         count = SSL_write(cSSL, buf+count, buf_size-count);
         //err = SSL_get_error(ssl_connection, count);
+
+        // the error handler below doesn't retry, it exits
+        //upon failure. Is there a more robust way to handle problems?
         CHK_SSL(count);
+        //will the below ever execute if the CHK_SSL above exits when count == -1?
         if (count < 0) {
             printf("ssl write count is: %d\n", count);
             count = -1;
@@ -83,7 +139,8 @@ int daapTCPLogWrite(char *buf, int buf_size) {
         total_count += count;
     }
 
-    daapTCPClose();
+    //daapTCPClose();
+    close(sockfd);
     pthread_mutex_unlock(&write_mutex);
     return count;
 }
@@ -93,7 +150,10 @@ int daapTCPConnect(void) {
   struct sockaddr_in servaddr;
   int ret_val = 0;
   int ssl_err = 0;
-  SSL_CTX *sslctx;
+  //SSL_CTX *sslctx; declared as static up top now
+
+  // code below moved to daapInitializeSSL()
+/*
   char ssl_client_cert[PATH_MAX];
   char ssl_client_key[PATH_MAX];
   int home_len;
@@ -118,8 +178,9 @@ int daapTCPConnect(void) {
       fprintf(stderr, "Invalid path length for certificates\n");
       return -1;
   }
-
-  daapInitializeSSL();
+*/
+  //call moved to daapInit
+  //daapInitializeSSL();
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if( sockfd < 0 ) {
       perror("socket creation failed");
@@ -141,12 +202,22 @@ int daapTCPConnect(void) {
       return -1;
   }
 
+  // code in comments below has been moved to daapInitializeSSL()
+  /*
   sslctx = SSL_CTX_new(SSLv23_client_method());
+  if (sslctx == NULL) {
+    ERROR_OUTPUT(("Error in creation of SSL_CTX object"));
+    return -1;
+  }
+  SSL_CTX_clear_options();
+  // recommended to not allow use of SSLv3 protocol for security reasons, so we turn it off
+  SSL_CTX_set_options(sslctx, SSL_OP_NO_SSLv3);
   int use_cert = SSL_CTX_use_certificate_file(sslctx, ssl_client_cert, 
 					      SSL_FILETYPE_PEM);
   int use_prv = SSL_CTX_use_PrivateKey_file(sslctx, ssl_client_key, 
 					    SSL_FILETYPE_PEM);
   SSL_CTX_set_verify(sslctx, SSL_VERIFY_NONE, NULL); 
+  */
   cSSL = SSL_new(sslctx);
   SSL_set_fd(cSSL, sockfd);
   ret_val = SSL_connect (cSSL);
