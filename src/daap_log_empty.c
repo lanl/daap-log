1736011eb040e73d9684247669a9165d20f5b3ae
Charles Shereda <cpshereda@lanl.gov>
2020-07-21 16:16:21 -0600

    Created stubs-like ("empty") API functions for new separate library Minor change to TCP/SSL stuff
    Signed-off-by: Charles Shereda <cpshereda@lanl.gov>

---
+++ src/daap_log_empty.c
@@ -0,0 +1,15 @@
+/* 
+ * Stubs-like (empty) version of daapLogWrite
+ * 
+ *****************
+ *
+ * Copyright (C) 2020 Triad National Security, LLC. All rights reserved.
+ * Original author: Charles Shereda, cpshereda@lanl.gov
+ * Additional author: Hugh Greenberg, hng@lanl.gov
+ */
+
+int daapLogWrite(const char *message, ...) {
+    va_list args;
+
+    return 0;
+}
